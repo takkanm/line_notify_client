2d7d154cbeb7b817df9b529c3b0016cd59b0f527
takkanm <takkanm@gmail.com>
2016-10-23 18:10:02 +0900

    first post message

--- lib/line_notify_client.ex
+++ lib/line_notify_client.ex
@@ -1,3 +1,20 @@
 defmodule LineNotifyClient do
-  
+  @url "https://notify-api.line.me/api/notify"
+
+  def post_message(token, message) do
+    body = "message=#{message}"
+    header = %{
+      "Content-Type":   "application/x-www-form-urlencoded",
+      "Authorization": "Bearer #{token}"
+    }
+
+    case HTTPoison.post(@url, body, header) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 400}} ->
+        :raise_400
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
 end
