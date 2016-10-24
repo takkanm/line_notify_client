4dbe2eb70b821e2da3a529ecd3a6211f1d982074
takkanm <takkanm@gmail.com>
2016-10-24 21:58:41 +0900

    extract method

--- lib/line_notify_client.ex
+++ lib/line_notify_client.ex
@@ -1,14 +1,17 @@
 defmodule LineNotifyClient do
   @url "https://notify-api.line.me/api/notify"
 
-  def post_message(token, message) do
-    body = "message=#{message}"
+  def post(token, message) do
+    post_message(token, %{message: message})
+  end
+
+  defp post_message(token, params) do
     header = %{
-      "Content-Type":   "application/x-www-form-urlencoded",
+      "Content-Type":  "application/x-www-form-urlencoded",
       "Authorization": "Bearer #{token}"
     }
 
-    case HTTPoison.post(@url, body, header) do
+    case HTTPoison.post(@url, URI.encode_query(params), header) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
       {:ok, %HTTPoison.Response{status_code: 400}} ->
