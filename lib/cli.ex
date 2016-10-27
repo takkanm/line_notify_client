db30d25442d324fbd8516ebbaebdfc7d07465d21
takkanm <takkanm@gmail.com>
2016-10-27 22:53:18 +0900

    support image options

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,5 +1,17 @@
 defmodule CLI do
-  def main([token, message]) do
-    LineNotifyClient.post(token, message)
+  def main(args) do
+    option_strict = [image_thumbnail: :string, image_fullsize: :string]
+    case OptionParser.parse(args, strict: option_strict) do
+      {[], [token|messages], _} ->
+        LineNotifyClient.post(token, Enum.join(messages, " "))
+      {[image_thumbnail: image_thumbnail, image_fullsize: image_fullsize], [token|messages], _} ->
+        LineNotifyClient.post(token, Enum.join(messages, " "), %{imageFullsize: image_fullsize, imageThumbnail: image_thumbnail})
+      _ ->
+        usage
+    end
+  end
+
+  def usage do
+    IO.puts "line_notify_client [--image_thubnail url --image_fullsize url] token msg1 [msg2...]"
   end
 end
