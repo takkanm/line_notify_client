b1a4c44bfd559bc957b86d7f6dfa7fc5a4c0e51b
takkanm <takkanm@gmail.com>
2016-11-15 21:23:29 +0900

    bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LineNotifyClient.Mixfile do
 
   def project do
     [app: :line_notify_client,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
