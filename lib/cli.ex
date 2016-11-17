3dc3a1bb18d052eccfbf880189ddee827ed35cb8
takkanm <takkanm@gmail.com>
2016-11-17 20:06:12 +0900

    support sticker

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,6 +1,11 @@
 defmodule CLI do
   def main(args) do
-    option_strict = [image_thumbnail: :string, image_fullsize: :string]
+    option_strict = [
+      image_thumbnail: :string,
+      image_fullsize: :string,
+      sticker_id: :integer,
+      sticker_package_id: :integer,
+    ]
     case OptionParser.parse(args, strict: option_strict) do
       {[], [token], _} ->
         LineNotifyClient.post_stdin(token)
@@ -24,6 +29,6 @@ defmodule CLI do
   end
 
   def usage do
-    IO.puts "line_notify_client [--image_thubnail url --image_fullsize url] token msg1 [msg2...]"
+    IO.puts "line_notify_client [--sticker_id number --sticker_package_id number][--image_thubnail url --image_fullsize url] token msg1 [msg2...]"
   end
 end
